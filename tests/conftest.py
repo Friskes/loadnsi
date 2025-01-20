04de0adcacf7af7ce74bbd34fa2102388bd958ea
Friskes <friskestv@gmail.com>
2025-01-20 10:43:40 +0300

    added override envs

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,10 @@
 import os
 
+from src.loadnsi.loadnsi import loadnsi  # noqa: F401
+
 
 def pytest_configure() -> None:
-    os.environ['PATH_TO_LOADNSI_CONFIG'] = 'tests/loadnsi_config.py'
-    os.environ['NSI_API_USER_KEY'] = 'some-key'
+    os.environ['PATH_TO_LOADNSI_CONFIG'] = os.environ.get(
+        'PATH_TO_LOADNSI_CONFIG', 'tests/loadnsi_config.py'
+    )
+    os.environ['NSI_API_USER_KEY'] = os.environ.get('NSI_API_USER_KEY', 'some-key')
